f68dd7a2efe6802174618f6add3d0b7c5f18faed
Syromiatnikov Vladimir <syromiatnikov.v@gmail.com>
2020-08-06 17:02:23 +0300

    basic template configure

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar color="primary" dark app>
+    <v-toolbar-title>
+      <v-icon left>mdi-camera</v-icon>
+      Photo Album App
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <v-icon left>mdi-cloud-upload</v-icon>
+      Загрузить
+    </v-btn>
+  </v-app-bar>
+</template>
---
+++ src/components/Photos.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fluid>
+    <v-card class="mt-1">
+      <v-container fluid>
+        <v-row>
+          <v-col
+            v-for="n in 15"
+            :key="n"
+            class="d-flex child-flex"
+            cols="6"
+            sm="4"
+            md="3"
+            lg="2"
+          >
+            <v-card flat tile class="d-flex">
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                aspect-ratio="1"
+                class="grey lighten-2"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
