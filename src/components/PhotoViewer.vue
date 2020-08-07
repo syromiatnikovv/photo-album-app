c3528a39795ea68a0b06c67b87db4f8fe2f2ba6c
Syromiatnikov Vladimir <syromiatnikov.v@gmail.com>
2020-08-07 15:01:59 +0300

    add v-viewer (view, resize, rotate ...)

---
+++ src/components/PhotoViewer.vue
@@ -0,0 +1,49 @@
+<template>
+  <viewer
+    :images="images"
+    :options="options"
+    @inited="inited"
+    class="viewer"
+    ref="viewer"
+  >
+    <img v-for="({ imageSrc }, index) in images" :key="index" :src="imageSrc" />
+  </viewer>
+</template>
+
+<script>
+import Vue from 'vue'
+import Viewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
+
+Vue.use(Viewer)
+
+export default {
+  props: ['images'],
+
+  data() {
+    return {
+      options: {
+        title: false,
+        movable: false,
+      },
+    }
+  },
+  methods: {
+    inited(viewer) {
+      this.$viewer = viewer
+    },
+
+    view(index) {
+      this.$viewer.view(index)
+    },
+  },
+}
+</script>
+
+<style>
+.viewer,
+.viewer > img,
+.viewer-loading > img {
+  display: none;
+}
+</style>
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,41 +1,46 @@
 <template>
   <v-container fluid :class="loading || !images.length ? 'fill-height' : ''">
-    <v-card v-if="!loading && !isNoPhotos" class="mt-1">
-      <v-container fluid>
-        <v-row>
-          <v-col
-            v-for="img in images"
-            :key="img.imageSrc"
-            class="d-flex child-flex"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <v-card flat tile class="d-flex">
-              <v-img
-                :src="img.imageSrc"
-                aspect-ratio="1"
-                class="grey lighten-2"
-              >
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-progress-circular
-                      indeterminate
-                      color="grey lighten-5"
-                    ></v-progress-circular>
-                  </v-row>
-                </template>
-              </v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <template v-if="!loading && !isNoPhotos">
+      <v-card class="mt-1">
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="({ imageSrc }, i) in images"
+              :key="i"
+              class="d-flex child-flex"
+              cols="6"
+              sm="4"
+              md="3"
+              lg="2"
+            >
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="imageSrc"
+                  aspect-ratio="1"
+                  class="grey lighten-2 photo-img"
+                  @click="viewImg(i)"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+
+      <photo-viewer :images="images" ref="viewer"></photo-viewer>
+    </template>
 
     <v-row
       v-else-if="!loading && isNoPhotos"
@@ -53,7 +58,13 @@
 </template>
 
 <script>
+import PhotoViewer from './PhotoViewer'
+
 export default {
+  components: {
+    PhotoViewer,
+  },
+
   data: () => ({
     loading: false,
   }),
@@ -84,6 +95,22 @@ export default {
         this.loading = false
       }
     },
+
+    viewImg(index) {
+      this.$refs.viewer.view(index)
+    },
   },
 }
 </script>
+
+<style>
+.photo-img {
+  cursor: pointer;
+  transition: box-shadow 0.2s;
+}
+
+.photo-img:hover {
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+}
+</style>
